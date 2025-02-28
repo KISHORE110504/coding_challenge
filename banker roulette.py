fefb57aa4d07453446ddcc3a21ad26874a509b88
KISHORE110504 <kishoreanbu70@gmail.com>
2025-02-28 15:48:43 +0530

    Stop tracking deleted files

--- banker roulette.py
+++
@@ -1,9 +0,0 @@
-import random
-
-names_string = input("Enter all family members name separated by comma and space:  ")
-names = names_string.split(", ")
-
-name = len(names)
-rand = random.randint(0, name - 1)
-
-print(f"{names[rand]} is going to buy the meal today!")
